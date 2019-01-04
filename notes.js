a7aeb4989cbe89672694bc1b6f309a4fb12dba0e
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-01-04 12:51:20 +0700

    Delete notes.js

--- notes.js
+++
@@ -1,9 +0,0 @@
-console.log('Starting notes.js');
-
-module.exports.addNote = function () {
-  console.log('addNote');
-  return 'สวัสดี';
-}
-module.exports.add =  function(a,b) {
-  return a + b;
-}
